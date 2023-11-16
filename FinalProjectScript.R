fc85217ebce6091ae97efd9c4313e7e61b22b26a
Andrew Myers <myersdrew32@gmail.com>
2023-11-16 09:49:06 -0500

    Uploading an R script before our group meeting

---
+++ FinalProjectScript.R
@@ -0,0 +1,11 @@
+###################################################
+#
+# Stat S610: Statistical Computing
+# Final Project Script
+#
+# Group: Drew, Katya, Soocheol
+#
+# Project: Forward stepwise selection 
+#
+###################################################
+
