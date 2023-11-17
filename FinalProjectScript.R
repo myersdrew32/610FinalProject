5106e9b5a3dd9c67a5fab8ef5a111b9cd0d9a20c
Andrew Myers <myersdrew32@gmail.com>
2023-11-17 07:48:59 -0500

    I added the prompt for the project, as well as our stated ideas for how to accomplish this from our past conversations

--- FinalProjectScript.R
+++ FinalProjectScript.R
@@ -9,3 +9,27 @@
 #
 ###################################################
 
+# Goal: Model selection and post-selection inference: 
+
+#	In regression modeling, we sometimes want to include only 
+#	a subset of our variables in the model and would like that 
+#	decision to be data dependent. Two solutions to this problem 
+#	are forward stepwise selection and backward stepwise
+#	selection, in which you fit a sequence of models, either 
+#	adding or subtracting one variable at a time until some 
+#	stopping criterion is reached. These procedures work for variable
+#	selection, but they invalidate the standard methods of 
+#	inference in linear models.
+
+# Idea from discussion: 
+#		Write a function that takes as arguments: (1) dataset, (2) response 
+#		variable, and (3) a list of predictor variables. The function 
+#		will then go through and model the response as a function
+#		of each predictor variable individually, keeping the one that reduces
+#		RSS the most. It will then go through and add variables in a similar
+#		fashion until (stopping criterion?)
+
+
+forwardstepwise <- function(data, response, predictors) {
+	# 
+}
